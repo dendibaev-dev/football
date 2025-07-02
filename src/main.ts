129e8c2d892b895829d441fe7d78c86555ee9060
dendibaev-dev <brunoalex2k19@gmail.com>
2025-07-02 15:38:21 +0500

    fx

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Название API')
+    .setDescription('Описание API')
+    .setVersion('1.0')
+    .addBearerAuth() // если используется JWT
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // Swagger доступен по /api
+
   app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3333);
 }
 bootstrap();
